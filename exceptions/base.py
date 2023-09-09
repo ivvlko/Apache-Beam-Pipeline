ae0fa520ef8018eda80a6bb62d159e0a19f94180
ivvlko <ivankoevbg@gmail.com>
2023-09-09 12:29:00 +0300

    add custom exceptions module

---
+++ exceptions/base.py
@@ -0,0 +1,8 @@
+"""
+A module for custom exceptions. We can define those here
+with just meaningful names and throw them in the validation process e.g.
+as well as adding logging or even alerts. 
+"""
+
+class DummyException(Exception):
+    pass
